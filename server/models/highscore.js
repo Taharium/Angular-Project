10e2afd3d505e99d32dafc5fcc9f64f2074f3bdd
Taharium <113913387+Taharium@users.noreply.github.com>
2023-06-20 17:54:55 +0200

    5 fertig

--- server/models/highscore.js
+++ server/models/highscore.js
@@ -4,4 +4,4 @@ const highscoreSchema = new mongoose.Schema({
     highscore: {type: Number, trim: true, default: '0'},
 });
 
-module.exports = mongoose.model('User', highscoreSchema);
+module.exports = mongoose.model('Highscore', highscoreSchema);
--- server/models/users.js
+++ server/models/users.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    company: {type: String, trim: true, default: 'FH Technikum Wien'},
+    company: {type: String, trim: true, default: 'FH Technikum Wien', required: true},
     street: {type: String, trim: true, default: ''},
     city: {type: String, trim: true, default: ''},
     postal: {type: String, trim: true, default: ''},
-    username: {type: String, trim: true, default: ''},
-    password: {type: String, trim: true, default: ''},
-    authToken: {type: String, trim: true, default: ''},
+    username: {type: String, trim: true, default: '', required: true},
+    password: {type: String, trim: true, default: '', required: true},
+    authToken: {type: String, trim: true, default: null},
 });
 
 module.exports = mongoose.model('User', userSchema);
